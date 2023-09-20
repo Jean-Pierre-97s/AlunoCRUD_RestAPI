fde8ca79b79acd6f61e75ea73359dedd1641d4c7
Jean Pierre <jean_pierre_97@outlook.com>
2023-09-20 16:20:13 -0300

    feat: added a way to save photos

--- src/modules/photos/infra/http/photos.controller.ts
+++ src/modules/photos/infra/http/photos.controller.ts
@@ -6,18 +6,68 @@ import {
   Patch,
   Param,
   Delete,
+  UploadedFile,
+  UseInterceptors,
+  ParseFilePipeBuilder,
+  HttpStatus,
 } from '@nestjs/common';
 import { PhotosService } from '../../service/photos.service';
 import { CreatePhotoDto } from '../../dto/create-photo.dto';
 import { UpdatePhotoDto } from '../../dto/update-photo.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
-@Controller('photos')
+@Controller('photo')
 export class PhotosController {
   constructor(private readonly photosService: PhotosService) {}
 
   @Post()
   create(@Body() createPhotoDto: CreatePhotoDto) {
     return this.photosService.create(createPhotoDto);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+  }
+
+  @Post('file')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          // Generate a custom filename here
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const newFilename = `${uniqueSuffix}${extname(file.originalname)}`;
+          callback(null, newFilename);
+        },
+      }),
+    }),
+  )
+  uploadFileAndPassValidation(
+    @Body() body: CreatePhotoDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'jpeg', // todo colocar outros formatos de imagem (|| 'jpg' || 'png')
+        })
+        .addMaxSizeValidator({
+          maxSize: 1024 * 1024 * 100,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return {
+      body,
+      file: file,
+    };
   }
 
   @Get()
